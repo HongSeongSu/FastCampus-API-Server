6df6e4b204c2305ce8437de6a95ce7fde5c4740f
Lee HanYeong <dev@azelf.com>
2017-03-03 06:46:51 +0900

    MEDIA_URL은 DEBUG일때만 동작하도록 수정

--- django_app/config/urls.py
+++ django_app/config/urls.py
@@ -21,4 +21,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
