14b5d0f27b53c1e34e86c8cb3d2934f0432a8cff
Lee HanYeong <dev@azelf.com>
2017-03-11 04:29:51 +0900

    SignupSerializer상속 수정

--- django_app/member/serializers.py
+++ django_app/member/serializers.py
@@ -1,3 +1,5 @@
+from rest_auth.registration.serializers import RegisterSerializer
+
 try:
     from allauth.account import app_settings as allauth_settings
     from allauth.utils import (email_address_exists,
@@ -6,12 +8,8 @@ try:
     from allauth.account.utils import setup_user_email
 except ImportError:
     raise ImportError("allauth needs to be added to INSTALLED_APPS.")
-from allauth.account.adapter import get_adapter
-from allauth.account.utils import setup_user_email
-from allauth.utils import get_username_max_length
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from rest_framework.exceptions import ParseError
 
 User = get_user_model()
 
@@ -28,37 +26,13 @@ class UserSerializer(serializers.ModelSerializer):
         )
 
 
-class SignupSerializer(serializers.Serializer):
-    username = serializers.CharField(
-        max_length=get_username_max_length(),
-        min_length=allauth_settings.USERNAME_MIN_LENGTH,
-        required=allauth_settings.USERNAME_REQUIRED
-    )
-    email = serializers.EmailField(required=allauth_settings.EMAIL_REQUIRED)
-    password1 = serializers.CharField(write_only=True)
-    password2 = serializers.CharField(write_only=True)
+class SignupSerializer(RegisterSerializer):
     first_name = serializers.CharField(max_length=30, required=True)
     last_name = serializers.CharField(max_length=30, required=True)
 
-    def custom_signup(self, request, user):
-        pass
-
     def get_cleaned_data(self):
-        return {
-            'username': self.validated_data.get('username', ''),
-            'password1': self.validated_data.get('password1', ''),
-            'email': self.validated_data.get('email', ''),
-            'first_name': self.validated_data.get('first_name', ''),
-            'last_name': self.validated_data.get('last_name', ''),
-        }
-
-    def save(self, request):
-        self.cleaned_data = self.get_cleaned_data()
-        if User.objects.filter(username=self.cleaned_data['username']).exists():
-            raise ParseError('username duplicate')
-        adapter = get_adapter()
-        user = adapter.new_user(request)
-        adapter.save_user(request, user, self)
-        self.custom_signup(request, user)
-        setup_user_email(request, user, [])
-        return user
+        ret = super().get_cleaned_data()
+        ret['first_name'] = self.validated_data['first_name']
+        ret['last_name'] = self.validated_data['last_name']
+        ret['email'] = self.validated_data.get('email', '')
+        return ret
